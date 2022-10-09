58ffeaf070f45014a8a5cd810bdd8efc6e562116
Va1er1y <webspay56@gmail.com>
2022-10-09 06:54:01 +0300

    pagination add

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-
+import  csv
+from pagination.settings import BUS_STATION_CSV
+from django.core.paginator import  Paginator
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
+txt = list()
+with open(BUS_STATION_CSV, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        txt.append(row)
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
+    page_namber = int(request.GET.get("page", 1))
+    paginator = Paginator(txt, 10)
+    page = paginator.get_page(page_namber)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': page,
+        'page': page,
     }
     return render(request, 'stations/index.html', context)
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -19,6 +19,48 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
+def omlet(request):
+    quanity = int(request.GET.get('servings', 1))
+    prelimary_recept = dict()
+    prelimary_recept.clear()
+    prelimary_recept['recipe'] = DATA.get('omlet').copy()
+    if quanity == 1:
+        context = prelimary_recept
+
+    else:
+        for key, value in prelimary_recept['recipe'].items():
+            prelimary_recept['recipe'][key] = value * quanity
+        context = prelimary_recept
+        print(context)
+        print(prelimary_recept)
+    return render(request, 'calculator/index.html', context)
+
+def pasta(request):
+    quanity = int(request.GET.get('servings', 1))
+    prelimary_recept = dict()
+    prelimary_recept['recipe'] = DATA.get('pasta').copy()
+    if quanity == 1:
+        context = prelimary_recept.copy()
+
+    else:
+        for key, value in prelimary_recept['recipe'].items():
+            prelimary_recept['recipe'][key] = value * quanity
+        context = prelimary_recept.copy()
+    return render(request, 'calculator/index.html', context)
+
+def buter(request):
+    quanity = int(request.GET.get('servings', 1))
+    prelimary_recept = dict()
+    prelimary_recept['recipe'] = DATA.get('buter').copy()
+    if quanity == 1:
+        context = prelimary_recept.copy()
+
+    else:
+        for key, value in prelimary_recept['recipe'].items():
+            prelimary_recept['recipe'][key] = value * quanity
+        context = prelimary_recept.copy()
+    return render(request, 'calculator/index.html', context)
+
 # Напишите ваш обработчик. Используйте DATA как источник данных
 # Результат - render(request, 'calculator/index.html', context)
 # В качестве контекста должен быть передан словарь с рецептом:
